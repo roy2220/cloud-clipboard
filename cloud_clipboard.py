69a5d20108d3662aeabb2cd59dfe8f20dc7da9c3
Roy O'Young <roy2220@outlook.com>
2016-11-19 18:52:24 +0800

    Refactor

--- cloud_clipboard.py
+++ cloud_clipboard.py
@@ -1,5 +1,8 @@
-from cloud_cat import CloudCat
+import json
+import requests
 import sys
+import time
+
 import config
 
 
@@ -8,14 +11,34 @@ def help():
 
 
 def get_cloud_clipboard():
-    cloud_cat = CloudCat(config.SALT)
-    text = cloud_cat.read()
-    return text
+    session = requests.Session()
+    auth = (config.PASTY_USER, config.PASTY_PASSWORD)
+
+    while True:
+        response = session.get(config.PASTY_API_ROOT_PATH + "clipboard/list.json", auth=auth)
+        response.raise_for_status()
+        data = json.loads(response.text)["payload"]
+        items = data["items"]
+
+        if len(items) == 0:
+            time.sleep(config.PASTY_CLIPBOARD_REFRESH_INTERVAL)
+        else:
+            item = items[-1]
+            item_id = item["_id"]
+            response = session.get(config.PASTY_API_ROOT_PATH + "clipboard/item/" + item_id, auth=auth)
+            response.raise_for_status()
+            data = json.loads(response.text)["payload"]
+            text = data["item"]
+            response = session.delete(config.PASTY_API_ROOT_PATH + "clipboard/item/" + item_id, auth=auth)
+            return text
 
 
 def set_cloud_clipboard(text):
-    cloud_cat = CloudCat(config.SALT)
-    cloud_cat.write(text)
+    auth = (config.PASTY_USER, config.PASTY_PASSWORD)
+    data = {"item": text}
+    response = requests.post(config.PASTY_API_ROOT_PATH + "clipboard/item/", auth=auth, json=data)
+    response.raise_for_status()
+    time.sleep(config.PASTY_CLIPBOARD_REFRESH_INTERVAL / 2)
 
 
 def main():
